b9152e7befce6cd9972cf6f6f5b279930b34433c
Victor Marcelino <victorfernandes@gec.inatel.br>
2018-11-12 01:06:17 -0200

    Added CommandReader class

--- KeyChainer.Core/command.py
+++ KeyChainer.Core/command.py
@@ -10,6 +10,36 @@ class Command:
         self.program.execute()
 
 
+
+class CommandReader:
+    def __init__(self, file=None):
+        self.filename = file
+
+
+    def read_commands(self, filename=None):
+        if filename is None and self.filename is None:
+            raise Exception('A filename must be supplied')
+
+        import json
+        cmds = []
+        with open(filename, 'r') as f:
+            dsrlz = json.loads(f.read())
+            for o in dsrlz:
+                cmds.append(Command(**o))
+
+        return cmds
+
+
+    def dump_commands(self, commands, filename=None):
+        if filename is None and self.filename is None:
+            raise Exception('A filename must be supplied')
+
+        import json
+        srlz = json.dumps([c.__dict__ for c in commands], IndentationError=4)
+        with open(filename, 'w+') as f:
+            f.write(srlz)
+
+
 class Program:
     def __init__(self, program_path, arguments, shell=False):
         self.program_path = program_path
