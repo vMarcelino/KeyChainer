ff1cf59dc5bb1b7892a09bbb93107c2326db3ed4
Victor Marcelino <victorfernandes@gec.inatel.br>
2018-11-12 02:12:21 -0200

    Added releasing of command-mode trigger keys and changed activation logic

--- KeyChainer.Core/input_processor.py
+++ KeyChainer.Core/input_processor.py
@@ -13,23 +13,35 @@ class InputProcessor:
             self.outside_keys_down = True
             self.keys_down = keys_down
 
+        self.allow_release = set()
+
 
     def on_key_down(self, key_name):
         if not self.outside_keys_down:
             self.keys_down.add(key_name)
 
-        self.key_chain += key_name
-        while len(self.key_chain) > 0:
-            is_valid = False
-            for command in self.commands:
-                if comamnd.activation_chain.startswith(self.key_chain):
-                    if command.activation_chain == self.key_chain:
-                        command.execute()
-                    elif len(command.activation_chain) > self.key_chain:
-                        is_valid = True
+        if set(['F1' ,'LControlKey']) == self.keys_down:
+            self.allow_release = self.keys_down.copy()
+            self.command_input_mode = not self.command_input_mode
 
-            if not is_valid:
-                self.key_chain = self.key_chain[1:]
+
+        if self.command_input_mode:
+            self.key_chain += key_name
+            valid = False
+            while len(self.key_chain) > 0 and not valid:
+                for command in self.commands:
+                    if command.activation_chain.startswith(self.key_chain):
+                        if command.activation_chain == self.key_chain:
+                            self.command_input_mode = False
+                            command.execute()
+                        elif len(command.activation_chain) > len(self.key_chain):
+                            valid = True
+
+                print(f'chain valid: {valid} ({self.key_chain})')
+                if not valid:
+                    self.key_chain = self.key_chain[1:]
+        else:
+            self.key_chain = ""
 
         return not self.command_input_mode
 
@@ -43,6 +55,10 @@ class InputProcessor:
             except Exception as ex:
                 import exception_handler
                 exception_handler.print_traceback(ex)
+
+        if key_name in self.allow_release:
+            self.allow_release.remove(key_name)
+            return True
 
         return not self.command_input_mode
 
